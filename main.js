5d21f75b236866650bb396f1588be9ba8204e984
Praveen Kumar <114238915+PraveenK50i@users.noreply.github.com>
2023-10-26 09:45:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/ETIR0UcOPF5RIvT2/scene.splinecode');
